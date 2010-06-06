4a6c9daff547019f81671e92e12ecfb5c38a4328
rodrigoyasuda <rodrigoyasuda@8734db39-eca5-8bd9-7945-d59b5fb66926>
2010-06-06 23:55:25 +0000

    mudando valor de retorno da task para String

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
-java -cp /home/rodrigoyasuda/Desktop/mc823-1s2010-rmi:/var/www/classes/compute.jar -Djava.rmi.server.codebase=file:/~/Desktop/mc823-1s2010-rmi/classes/ -Djava.security.policy=client.policy client.ComputePi localhost $1
+java -cp /home/rodrigoyasuda/Desktop/mc823-1s2010-rmi:/var/www/classes/compute.jar -Djava.rmi.server.codebase=file:/~/Desktop/mc823-1s2010-rmi/classes/ -Djava.security.policy=client.policy client.ComputePi localhost $1 $2 $3
 
