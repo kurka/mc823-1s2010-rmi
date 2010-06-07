4b85895320b753d8b62b1a2991d8c5ff42273c3c
rodrigoyasuda <rodrigoyasuda@8734db39-eca5-8bd9-7945-d59b5fb66926>
2010-06-07 00:14:20 +0000

    marcando tempo do cliente

--- client/ComputePi.java
+++ client/ComputePi.java
@@ -2,6 +2,7 @@ package client;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.*;
 import compute.Compute;
 
 public class ComputePi {
@@ -14,10 +15,17 @@ public class ComputePi {
             Registry registry = LocateRegistry.getRegistry(args[0]);
             Compute comp = (Compute) registry.lookup(name);
             Pi task = new Pi(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
-            /* Marcar tempo do cliente aqui */
+
+            /* Marca inicio do tempo do cliente aqui */
+            long start = System.nanoTime(); 
             String pi = comp.executeTask(task);
             /* Terminar tempo do cliente aqui */
+            long end = System.nanoTime();
+            /* Imprime resultado */
             System.out.println(pi);
+            /* Imprime tempo do cliente */
+            System.out.format("TOTALTIME\t%.6f\n", (end-start)/1000000000F);
+
         } catch (Exception e) {
             System.err.println("ComputePi exception:");
             e.printStackTrace();
