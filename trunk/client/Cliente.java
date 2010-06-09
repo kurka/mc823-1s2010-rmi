f658a6504f28bd7e7230066d750fac0994ef0d2f
david.kurka@gmail.com <david.kurka@gmail.com@8734db39-eca5-8bd9-7945-d59b5fb66926>
2010-06-09 02:37:15 +0000

    implementado mais uma parte. main ta praticamente pronto, falta funcoes auxiliares

--- trunk/client/Cliente.java
+++ trunk/client/Cliente.java
@@ -14,16 +14,11 @@ public class Cliente {
 		int sentbytes = 0, recvbytes = 0, connfd = 0;
 		/* buffers de entrada e saida */
 		String in_buffer, out_buffer;
-		/* buffer para guardar a opcao selecionada */
-		char[] opcao = {'\0', '\0'}
 		/* variaveis para guardar a nota e id do filme */
-		int nota = -1, id = 0;
-		/* variaveis para controle do tempo */
-  
-		/* variaveis para resolucao de nomes e conexao com o servidor */
-		//struct hostent* dns_resolver;
-		//struct sockaddr_in ssock;
-
+		int nota = -1, id = 0, opcao = -1;
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));  
+		
 		String name = "Compute";
 		Registry registry = LocateRegistry.getRegistry(args[0]);
 		Compute comp = (Compute) registry.lookup(name);
@@ -36,7 +31,7 @@ public class Cliente {
   
 		/* Obter argumentos da linha de comando */
 		if (args.lenght > 1)
-			opcao[0] = args[1][0];
+			opcao = Integer.parseInt(args[1])
 		if (args.lenght > 2)
 			id = Integer.parseInt(args[2]);
 		if (args.lenght > 3)
@@ -47,49 +42,50 @@ public class Cliente {
 		//cleanbuffer(out_buffer);
   
   
-		if (opcao[0] == '\0')
+		if (opcao[0] == -1)
 		{
 			printmenu();
 
-			//opcao[0] = getchar();
-			//while(getchar() != '\n');
+			String input = reader.readLine();  
+			opcao = Integer.parseInt(input);  
 		}
   
-		/*switch(atol(opcao))*/
-		/*{*/
-			/*case REQUEST_ALL:*/
-				/*sprintf(out_buffer, "%d", REQUEST_ALL);*/
-				/*break;*/
-			/*case REQUEST_ONE:*/
-				/*if (id == 0)*/
-					/*id = getint("Digite o ID do filme:");*/
-				/*sprintf(out_buffer, "%d %d", REQUEST_ONE, id);*/
-				/*break;*/
-			/*case REQUEST_TITLES:*/
-				/*sprintf(out_buffer, "%d", REQUEST_TITLES);*/
-				/*break;*/
-			/*case REQUEST_PLOT:*/
-				/*if (id == 0)*/
-					/*id = getint("Digite o ID do filme:");*/
-				/*sprintf(out_buffer, "%d %d", REQUEST_PLOT, id);*/
-				/*break;*/
-			/*case REQUEST_VOTE:*/
-				/*if (id == 0)*/
-					/*id = getint("Digite o ID do filme:");*/
-				/*if (nota == -1)*/
-					/*while ((nota = getint("Digite uma nota de 0 a 10:")) > 10);*/
-				/*sprintf(out_buffer, "%d %d %d", REQUEST_VOTE, id, nota);*/
-				/*break;*/
-			/*case REQUEST_RATE:*/
-				/*if (id == 0)*/
-					/*id = getint("Digite o ID do filme:");*/
-				/*sprintf(out_buffer, "%d %d", REQUEST_RATE, id);*/
-				/*break;*/
-			/*default:*/
-				/*return 0;*/
-				/*break;*/
-		/*}*/
-  
+		switch(opcao)
+		{
+			case REQUEST_ALL:
+				out_buffer = "" + REQUEST_ALL;
+				break;
+			case REQUEST_ONE:
+				if (id == 0)
+					id = getint("Digite o ID do filme:");
+				out_buffer = REQUEST_ONE + " " + id;
+				break;
+			case REQUEST_TITLES:
+				out_buffer = "" + REQUEST_TITLES;
+				break;
+			case REQUEST_PLOT:
+				if (id == 0)
+					id = getint("Digite o ID do filme:");
+				out_buffer = REQUEST_PLOT + " " + id;
+				break;
+			case REQUEST_VOTE:
+				if (id == 0)
+					id = getint("Digite o ID do filme:");
+				if (nota == -1)
+					while ((nota = getint("Digite uma nota de 0 a 10:")) > 10);
+				out_buffer = REQUEST_PLOT + " " + id + " " + nota;
+				break;
+			case REQUEST_RATE:
+				if (id == 0)
+					id = getint("Digite o ID do filme:");
+				out_buffer = REQUEST_RATE + " " + id;
+				break;
+			default:
+				return;
+				break;
+		}
+ 
+		//criar o task!
         String ret = comp.executeTask(task);
   
 		return(0);
