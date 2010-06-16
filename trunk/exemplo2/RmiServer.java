14a6d79c43158b8ec04f3b5d90fec9f5c8d69708
rodrigoyasuda <rodrigoyasuda@8734db39-eca5-8bd9-7945-d59b5fb66926>
2010-06-16 19:37:39 +0000

    script compilacao

--- trunk/exemplo2/RmiServer.java
+++ trunk/exemplo2/RmiServer.java
@@ -2,22 +2,76 @@ import java.rmi.*;
 import java.rmi.registry.*;
 import java.rmi.server.*;
 import java.net.*;
-
- 
+import java.util.Formatter;
+import java.io.Serializable;
+import java.sql.*;
 
 public class RmiServer extends java.rmi.server.UnicastRemoteObject
-implements ReceiveMessageInterface
+implements cinemaInterface
 {
     int      thisPort = 50000;
     String   thisAddress;
     Registry registry;    // rmi registry for lookup the remote objects.
     // This method is called from the remote client by the RMI.
     // This is the implementation of the “ReceiveMessageInterface”.
+    private static final String userName = "rodrigo_yasuda";
+    private static final String password = "teste123";
+    private static final String url = "jdbc:mysql://mysql.tolstenko.net:3306/rodrigo_yasuda?useUnicode=yes&characterEncoding=UTF-8&user=" + userName + "&password=" + password;
 
-    public String receiveMessage(int opcao, int id, int nota) throws RemoteException
+    public String TodosDados(int opcao, int id, int nota)
     {
-        System.out.println(opcao);
-        return "teste";
+    Connection conn = null;
+    try {
+      /* Conexao com servidor MySQL */
+      Class.forName ("com.mysql.jdbc.Driver").newInstance();
+      conn = DriverManager.getConnection(url);
+      //System.out.println("Connected to the database");
+      conn.close();
+      //System.out.println("Disconnected from database");
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      System.err.println ("Cannot connect to database server");
+    }
+    finally {
+      if (conn != null)
+      {
+        try {
+          conn.close ();
+          //System.out.println ("Database connection terminated");
+        } catch (Exception e) { /* ignore close errors */ }
+      }
+    }
+    
+    }
+
+    public String consulta(int opcao, int id, int nota) throws RemoteException
+    {
+      /* Inicio do tempo do servidor aqui */
+      long start = System.nanoTime();
+      /* Processar pedido aqui */
+      String resposta = new String();
+      System.out.println("Server received: " + opcao + " " + id + " " + nota);
+    
+
+      switch(opcao)
+      {
+        case 1:
+          resposta = "Todos filmes";
+          break;
+        default:
+          resposta = "Opcao incorreta";
+          break;
+      }
+      System.out.println(opcao);
+
+      /* Termino do tempo do servidor aqui */
+      long end = System.nanoTime();
+      double dif = (end - start)/1000000000F;
+      resposta = resposta + "SERVERTIME\t" + dif;
+      System.out.println(resposta);
+    
+      return resposta;
     }
  
     public RmiServer() throws RemoteException
