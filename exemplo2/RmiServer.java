754b6f51bb15e41ed42754d6c1fd970874380efe
rodrigoyasuda <rodrigoyasuda@8734db39-eca5-8bd9-7945-d59b5fb66926>
2010-06-16 19:18:25 +0000

    Modificando interface

--- exemplo2/RmiServer.java
+++ exemplo2/RmiServer.java
@@ -10,11 +10,8 @@ public class RmiServer extends java.rmi.server.UnicastRemoteObject
 implements ReceiveMessageInterface
 
 {
-
-    int      thisPort;
-
+    int      thisPort = 50000;
     String   thisAddress;
-
     Registry registry;    // rmi registry for lookup the remote objects.
 
  
@@ -23,76 +20,41 @@ implements ReceiveMessageInterface
 
     // This is the implementation of the “ReceiveMessageInterface”.
 
-    public void receiveMessage(String x) throws RemoteException
-
+    public String receiveMessage(int opcao, int id, int nota) throws RemoteException
     {
-
-        System.out.println(x);
-
+        System.out.println(opcao);
+        return "teste";
     }
-
  
-
     public RmiServer() throws RemoteException
-
     {
-
         try{
-
             // get the address of this host.
-
             thisAddress= (InetAddress.getLocalHost()).toString();
-
         }
-
         catch(Exception e){
-
             throw new RemoteException("can't get inet address.");
-
         }
-
-thisPort=3232;  // this port(registry’s port)
 
         System.out.println("this address="+thisAddress+",port="+thisPort);
-
         try{
-
         // create the registry and bind the name and object.
-
         registry = LocateRegistry.createRegistry( thisPort );
-
             registry.rebind("rmiServer", this);
-
         }
-
         catch(RemoteException e){
-
         throw e;
-
         }
-
     }
-
    
-
     static public void main(String args[])
-
     {
-
         try{
-
         RmiServer s=new RmiServer();
-
     }
-
     catch (Exception e) {
-
            e.printStackTrace();
-
            System.exit(1);
-
     }
-
      }
-
 }
